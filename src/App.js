b7c34d7fb0c01138faa6933ffab2efe138a65c95
minimalentry <minimalentry@gmail.com>
2017-05-07 18:04:24 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Container>
+              <h1>Hello world.</h1>
+          </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
