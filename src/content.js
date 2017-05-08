82b20f0f7720cdfdf53f0cb96cdce5e3ca70e1c3
minimalentry <minimalentry@gmail.com>
2017-05-08 04:50:22 -0500

    Added resume contents

--- src/content.js
+++ src/content.js
@@ -4,7 +4,7 @@ export default {
         jobTitle: 'Software Engineer',
         overview: {
             avatar: 'images/avatar.jpg',
-            text: 'Lorem ipsum dolor sit amet, elitr legere id qui, est ex facer eirmod consequat. Has purto repudiandae id. In eum dico ullamcorper delicatissimi, mei quod accusam in. No tota gloriatur usu, sit labores rationibus definiebas ad. Aeque sanctus moderatius eu qui. At doming tamquam abhorreant ius, ea esse probo facer nam, semper malorum ne est. Ne ancillae scribentur sit. Et sed diceret corpora definitionem, laoreet interesset ea vel, eleifend pertinacia cu his. Quo odio facete nusquam ex, vidit tempor noster ei his. Tation dolores sadipscing ut pro, est ne commodo assueverit.'
+            text: 'A future-minded, goal-oriented technology enthusiast who goes home after a day of coding only to jump back on and work on personal projects. A strong attention to detail and a passion for success through teamwork combine with a legendary work ethic to produce a quick learner well suited for the challenges of modern software development.'
         }
     },
     content: [
@@ -88,18 +88,22 @@ export default {
                     image: 'images/dealersocket.png',
                     heading: 'Software Engineer at DealerSocket',
                     text: [
-                        'From August 8th, 2017 to present',
-                        'Lorem ipsum dolor sit amet, elitr legere id qui, est ex facer eirmod consequat. Has purto repudiandae id. In eum dico ullamcorper delicatissimi, mei quod accusam in. No tota gloriatur usu, sit labores rationibus definiebas ad. Aeque sanctus moderatius eu qui. At doming tamquam abhorreant ius, ea esse probo facer nam, semper malorum ne est.',
-                        'Ne ancillae scribentur sit. Et sed diceret corpora definitionem, laoreet interesset ea vel, eleifend pertinacia cu his. Quo odio facete nusquam ex, vidit tempor noster ei his. Tation dolores sadipscing ut pro, est ne commodo assueverit.'
+                        'April 2016 to present',
+                        'Develop progressive, scalable, responsive single page applications that consume JSON data and output React components reacting to state changes issued through Redux',
+                        'Collaborate in a tight-knit Agile Scrum environment by utilizing Atlassian JIRA to create user stories and follow documentation standards in Atlassian Confluence, while taking part in ceremonies to strengthen inter-team bonds',
+                        'Utilize Grunt and Angular.js to perform maintenance and unit testing on a key application module',
+                        'Update and maintain legacy code and transform it into modern code via consistent refactoring efforts',
+                        'Build solutions with a strong, test-driven approach for maximum automation capability using Mocha/Chai/Sinon and Jest for React components Contribute to the various artifact projects spread across the company in a code-unifying effort across all teams, particularly through use of versioning control with git and BitBucket'
                     ]
                 },
                 {
                     image: 'images/flowtrac.jpg',
                     heading: 'Mobile Software Engineer at FlowTrac',
                     text: [
-                        'From April 4th, 2016 to July 25th, 2017',
-                        'Lorem ipsum dolor sit amet, elitr legere id qui, est ex facer eirmod consequat. Has purto repudiandae id. In eum dico ullamcorper delicatissimi, mei quod accusam in. No tota gloriatur usu, sit labores rationibus definiebas ad. Aeque sanctus moderatius eu qui. At doming tamquam abhorreant ius, ea esse probo facer nam, semper malorum ne est.',
-                        'Ne ancillae scribentur sit. Et sed diceret corpora definitionem, laoreet interesset ea vel, eleifend pertinacia cu his. Quo odio facete nusquam ex, vidit tempor noster ei his. Tation dolores sadipscing ut pro, est ne commodo assueverit.'
+                        'April 2016 to July 2017',
+                        'Created cross-platform mobile applications for iOS and Android by utilizing Appcelerator Titanium SDK',
+                        'Integrated customer warehouse data into the mobile UI and devised a scheme to keep an offline cache to improve performance for areas which may be out of service',
+                        'Performed automated testing complete with branching statements for both the iOS and Android paths'
                     ]
                 },
             ]
--- src/environment/components/Overview/Overview.jsx
+++ src/environment/components/Overview/Overview.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Segment, Item } from 'semantic-ui-react';
+import { Container, Segment, Item, Header, Comment } from 'semantic-ui-react';
 import './Overview.css';
 
 export default class Overview extends Component {
@@ -17,7 +17,19 @@ export default class Overview extends Component {
                                 <Item.Image src={avatar} size='medium'/>
                                 <Item.Content>
                                     <Item.Header>Overview</Item.Header>
-                                    <Item.Description>{text}</Item.Description>
+                                    <Item.Description className='description'>{text}</Item.Description>
+                                    <Segment>
+                                        <Comment.Group>
+                                            <Comment>
+                                                <Comment.Avatar src='/images/susan.jpg' />
+                                                <Comment.Content>
+                                                    <Comment.Author>Susan Owens</Comment.Author>
+                                                    <Comment.Metadata>March 2016</Comment.Metadata>
+                                                    <Comment.Text>"I would recommend Connor to assist you with growth and expansion of your business by means of technology. He was able to assist me with a solution to communicate with clients and staff via a secure server. We can upload files, video and other secure documents safely."</Comment.Text>
+                                                </Comment.Content>
+                                            </Comment>
+                                        </Comment.Group>
+                                    </Segment>
                                 </Item.Content>
                             </Item>
                         </Item.Group>
