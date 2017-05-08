aeb34bcc469c1a2650f4a751488aae7cad63265d
minimalentry <minimalentry@gmail.com>
2017-05-07 21:16:09 -0500

    SectionMenu now sticks when at the correct height

--- src/environment/components/SectionMenu/SectionMenu.jsx
+++ src/environment/components/SectionMenu/SectionMenu.jsx
@@ -4,11 +4,39 @@ import S from 'string';
 import './SectionMenu.css';
 
 export default class extends Component {
+    constructor (props) {
+        super(props);
+        this.state = { stuck: false };
+        this.setMenuStickOnScroll();
+    }
+
+    stick () {
+        this.setState({ stuck: true });
+    }
+
+    unstick () {
+        this.setState({ stuck: false });
+    }
+
+    setMenuStickOnScroll () {
+        window.addEventListener('scroll', () => {
+            const offset = window.pageYOffset;
+            const mainOffset = document.getElementById('main').offsetTop;
+            offset >= mainOffset ? this.stick() : this.unstick();
+        });
+    }
+
     render () {
+        const { stuck } = this.state;
         const { active, collection } = this.props;
+        const fixed = stuck ? 'top' : null;
         return (
             <div className='SectionMenu'>
-                <Menu widths={3} pointing>
+                <div id='main' />
+                <Menu
+                    widths={3}
+                    fixed={fixed}
+                    pointing>
                     {collection.map((section, index) => (
                         <Menu.Item
                             key={section.name}
